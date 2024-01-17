e5b5170f483643d0e247fc25fba1c46fc5dd8dd1
Sorada Prathan <sorada1111@gmail.com>
2024-01-17 14:36:33 -0500

    initial commit

---
+++ apps/toyhubshop/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'toys-hub-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'toyhubshop';
+}
