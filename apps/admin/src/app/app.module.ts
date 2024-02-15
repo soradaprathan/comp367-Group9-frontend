b4eaaf32d972c1de73f5cd933d92da937ff2d9ee
Sorada Prathan <sprathan@my.centennialcollege.ca>
2024-02-14 23:03:22 -0500

    Create Product Table Page and add/edit Product Table Page closes #2, closes #3

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,12 +1,49 @@
+/* eslint-disable max-len */
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { ShellComponent } from './shared/shell/shell.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProductsListComponent } from './pages/products/products-list/products-list.component';
+import { ProductsFormComponent } from './pages/products/products-form/products-form.component';
 
+import { CardModule } from 'primeng/card';
+import { ToolbarModule } from 'primeng/toolbar';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { ToastModule } from 'primeng/toast';
+
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { EditorModule } from 'primeng/editor';
+
+const UX_MODULE = [
+    CardModule,
+    ToastModule,
+    InputTextModule,
+    TableModule,
+    ToolbarModule,
+    ButtonModule,
+    ConfirmDialogModule,
+    ColorPickerModule,
+    InputNumberModule,
+    DropdownModule,
+    InputTextareaModule,
+    InputSwitchModule,
+    EditorModule
+];
 const routes: Routes = [
     {
         path: '',
@@ -15,15 +52,43 @@ const routes: Routes = [
             {
                 path: 'dashboard',
                 component: DashboardComponent
+            },
+            {
+                path: 'products',
+                component: ProductsListComponent
+            },
+            {
+                path: 'products/form',
+                component: ProductsFormComponent
+            },
+            {
+                path: 'products/form/:id',
+                component: ProductsFormComponent
+            },
+            {
+                path: 'categories',
+                component: DashboardComponent
+            },
+            {
+                path: 'orders',
+                component: DashboardComponent
             }
         ]
     }
 ];
 
 @NgModule({
-    declarations: [AppComponent, DashboardComponent, ShellComponent, SidebarComponent],
-    imports: [BrowserModule, RouterModule.forRoot(routes, { initialNavigation: 'enabled' })],
-    providers: [],
+    declarations: [AppComponent, SidebarComponent, ShellComponent, DashboardComponent, ProductsListComponent, ProductsFormComponent],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot(routes, { initialNavigation: 'enabled' }),
+        ...UX_MODULE
+    ],
+    providers: [MessageService, ConfirmationService],
     bootstrap: [AppComponent]
 })
 export class AppModule {}
