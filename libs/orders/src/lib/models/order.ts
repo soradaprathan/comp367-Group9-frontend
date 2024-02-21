5c69c3ed8991db083c21cf1b47583200e9b8dce4
Jasper Jan Tan <jasperjantan1996@gmail.com>
2024-02-21 13:03:27 -0500

    Orders List and Show Orders Form

---
+++ libs/orders/src/lib/models/order.ts
@@ -0,0 +1,17 @@
+import { OrderItem } from './order-item';
+import { User } from '@toys-hub/users';
+
+export class Order {
+  id?: string;
+  orderItems?: OrderItem;
+  shippingAddress1?: string;
+  shippingAddress2?: string;
+  city?: string;
+  zip?: string;
+  country?: string;
+  phone?: string;
+  status?: string;
+  totalPrice?: string;
+  user?: User;
+  dateOrdered?: string;
+}
