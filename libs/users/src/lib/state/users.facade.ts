b8c54aba4689480018043d62bfc9491566ef9a43
Lanh Hoang <lanhhoang@users.noreply.github.com>
2024-03-24 17:46:06 -0400

    Observe StateStore Fields Using Selectors

--- libs/users/src/lib/state/users.facade.ts
+++ libs/users/src/lib/state/users.facade.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@angular/core';
 
-import { select, Store, Action } from '@ngrx/store';
+import { select, Store } from '@ngrx/store';
 
 import * as UsersActions from './users.actions';
-import * as UsersFeature from './users.reducer';
 import * as UsersSelectors from './users.selectors';
 
 @Injectable()
 export class UsersFacade {
+
+    currentUser$ = this.store.pipe(select(UsersSelectors.getUser));
+    isAuthenticated$ = this.store.pipe(select(UsersSelectors.getUserIsAuth));
+
     constructor(private store: Store) {}
 
     buildUserSession() {
--- libs/users/src/lib/state/users.selectors.ts
+++ libs/users/src/lib/state/users.selectors.ts
@@ -1,19 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { USERS_FEATURE_KEY, State, UsersPartialState, usersAdapter } from './users.reducer';
+import { USERS_FEATURE_KEY, UsersPartialState, UsersState  } from './users.reducer';
 
 // Lookup the 'Users' feature state managed by NgRx
-export const getUsersState = createFeatureSelector<UsersPartialState, State>(USERS_FEATURE_KEY);
+export const getUsersState = createFeatureSelector<UsersPartialState, UsersState>(USERS_FEATURE_KEY);
 
-const { selectAll, selectEntities } = usersAdapter.getSelectors();
+export const getUser = createSelector(getUsersState, (state) => state.user);
 
-export const getUsersLoaded = createSelector(getUsersState, (state: State) => state.loaded);
-
-export const getUsersError = createSelector(getUsersState, (state: State) => state.error);
-
-export const getAllUsers = createSelector(getUsersState, (state: State) => selectAll(state));
-
-export const getUsersEntities = createSelector(getUsersState, (state: State) => selectEntities(state));
-
-export const getSelectedId = createSelector(getUsersState, (state: State) => state.selectedId);
-
-export const getSelected = createSelector(getUsersEntities, getSelectedId, (entities, selectedId) => selectedId && entities[selectedId]);
+export const getUserIsAuth = createSelector(getUsersState, (state) => state.isAuthenticated);
