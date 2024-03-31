b4b98882df0a26ae263f82794329edb702d0032e
shield0225 <shieldsalcedo@yahoo.com>
2024-03-31 16:16:01 -0400

    updated orders libs

--- libs/orders/src/lib/services/orders.service.ts
+++ libs/orders/src/lib/services/orders.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 import { Order } from '../models/order';
+import { OrderItem } from '../models/order-item';
 import { environment } from '@env/environment';
+import { StripeService } from 'ngx-stripe';
 
 @Injectable({
     providedIn: 'root'
@@ -12,7 +14,7 @@ export class OrdersService {
     apiURLOrders = environment.apiUrl + 'orders';
     apiURLProducts = environment.apiUrl + 'products';
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private stripeService: StripeService) {}
 
     getOrders(): Observable<Order[]> {
         return this.http.get<Order[]>(this.apiURLOrders);
@@ -44,8 +46,14 @@ export class OrdersService {
     getProduct(productId: string): Observable<any> {
         return this.http.get<any>(`${this.apiURLProducts}/${productId}`);
       }
-    
+
     getOrdersByUserId(userId: string): Observable<Order[]> {
         return this.http.get<Order[]>(`${this.apiURLOrders}/get/userorders/${userId}`);
+    }
+
+    createCheckoutSession(orderItem: OrderItem[]) {
+        return this.http.post(`${this.apiURLOrders}/create-checkout-session`, orderItem).pipe(switchMap((session: {id:string}) => {
+            return this.stripeService.redirectToCheckout({sessionId: session.id});
+        }));
     }
 }
